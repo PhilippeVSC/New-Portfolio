389e3729efdf1b6d9602cf8e44022def086c403a
PhilippeVSC <philippe.vivesc@gmail.com>
2023-04-02 20:35:59 +0200

    completed header with comments

--- script.js
+++ script.js
@@ -1,11 +1,15 @@
+/* Import some variable */
 const menu_burger_button = document.getElementsByClassName('menu-burger')[0];
 const nav_ul = document.getElementsByClassName('nav-ul')[0];
+const header = document.getElementsByClassName('header')[0];
 
-
+/* When burger menu is clicked */
 menu_burger_button.addEventListener('click', () => {
     if(nav_ul.classList.contains('active')){
         nav_ul.classList.remove('active');
+        header.classList.remove('active');
     } else {
         nav_ul.classList.add('active');
+        header.classList.add('active');
     }
 });
