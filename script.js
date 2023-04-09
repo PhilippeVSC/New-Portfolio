65ef9a8bbf673caafa3087ee2441664786fed216
PhilippeVSC <philippe.vivesc@gmail.com>
2023-04-09 04:21:36 +0200

    Home page with animtion (not finished)

--- script.js
+++ script.js
@@ -2,3 +2,11 @@ function ShowHeader() {
     var header = document.querySelector('header');
     header.classList.toggle('active');
 }
+
+
+var wordpress = document.getElementById('wordpress');
+var html = document.getElementById('html');
+var css = document.getElementById('css');
+var javascript = document.getElementById('javascript');
+
+var icons = [wordpress, html, css, javascript]; /* Icons to animate */
