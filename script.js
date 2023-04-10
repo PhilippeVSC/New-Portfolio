caa046b6c2497211ddfad96fcc9d30c10865dc2d
PhilippeVSC <philippe.vivesc@gmail.com>
2023-04-11 01:28:56 +0200

    projects section no responsive

--- script.js
+++ script.js
@@ -58,3 +58,15 @@ icons.forEach(function(icon, index) {
     style.innerHTML = keyframes;
     document.head.appendChild(style);
 });
+
+// Card light effect function
+document.getElementById("cards").onmousemove = e => {
+    for(const card of document.getElementsByClassName("card")) {
+      const rect = card.getBoundingClientRect(),
+            x = e.clientX - rect.left,
+            y = e.clientY - rect.top;
+  
+      card.style.setProperty("--mouse-x", `${x}px`);
+      card.style.setProperty("--mouse-y", `${y}px`);
+    };
+  }
